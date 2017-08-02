5c46b5733071106fb4990074c14c239b46e63923
jjz921024 <470623352@qq.com>
2017-08-03 00:00:55 +0800

    init

---
+++ src/list/ReverstList.java
@@ -0,0 +1,12 @@
+package list;
+
+import utils.ListNode;
+
+/**
+ * Created by Jun on 2017/8/2.
+ */
+public class ReverstList {
+    public ListNode reverseList(ListNode head) {
+        return null;
+    }
+}
