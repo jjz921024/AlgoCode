9fd9aa459b96dec48516e715e0a37339c50d404a
jjz921024 <470623352@qq.com>
2017-08-24 17:37:08 +0800

    no finish Palindrome & Printlistfromtail111

--- src/question/linkedlist/PrintListFromTail.java
+++ src/question/linkedlist/PrintListFromTail.java
@@ -6,34 +6,38 @@ import java.util.Stack;
 
 
 /**
- * https://www.nowcoder.com/questionCenter?questionTypes=000100&mutiTagIds=580
  * Created by Jun on 2017/8/2.
- *
- *  从尾到头打印链表
- *  https://www.nowcoder.com/questionTerminal/d0267f7f55b3412ba93bd35cfa8e8035?orderByHotValue=1&questionTypes=000100&mutiTagIds=580&page=1&onlyReference=false
+ * 从尾到头打印链表
  */
 public class PrintListFromTail {
-    ArrayList<Integer> result = new ArrayList<>();
-    public ArrayList<Integer> printListFromTailToHead(Node listNode) {
+
+    /**
+     * 递归方法，把链表从尾到头依次添加进数组
+     * 打印数组
+     */
+    private static ArrayList<Integer> result = new ArrayList<>();
+    public static ArrayList<Integer> printListFromTailToHead1(Node listNode) {
         if (listNode != null) {
-            printListFromTailToHead(listNode.next);
+            printListFromTailToHead1(listNode.next);
             result.add(listNode.val);
         }
         return result;
     }
 
-
-    //栈
-    public ArrayList<Integer> printListFromTailToHead1(Node listNode) {
+    /**
+     * 将链表依次入栈，再将栈依次弹出
+     */
+    public static ArrayList<Integer> printListFromTailToHead2(Node listNode) {
         Stack<Integer> stack = new Stack<>();
         while (listNode != null) {
             stack.push(listNode.val);
             listNode = listNode.next;
         }
-        ArrayList<Integer> integers = new ArrayList<>();
+
+        ArrayList<Integer> result = new ArrayList<>();
         while (!stack.empty()) {
-            integers.add(stack.pop());
+            result.add(stack.pop());
         }
-        return integers;
+        return result;
     }
 }
