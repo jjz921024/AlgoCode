c194aa5869599630d750ec7751ef22de9e97d849
jjz921024 <470622352@qq.com>
2020-01-14 09:32:46 +0800

    字符串匹配 Sunday

--- src/main/java/algo/string/StringMatch.java
+++ src/main/java/algo/string/StringMatch.java
@@ -13,18 +13,15 @@ public class StringMatch {
      * 复杂度 O(mn)
      */
     public static int matchByBF(String str, String pattern) {
-        for (int i = 0; i < str.toCharArray().length; i++) {
-            // 记录主串位置
-            int ii = i;
-            int j;
-            for (j = 0; j < pattern.toCharArray().length; j++) {
-                if (str.charAt(ii++) != pattern.charAt(j)) {
+        for (int i = 0; i < str.length() - pattern.length(); i++) {
+            for (int j = 0; j < pattern.length(); j++) {
+                if (str.charAt(i + j) != pattern.charAt(j)) {
                     break;
                 }
-            }
-            // 是否遍历完模式串
-            if (j == pattern.length()) {
-                return i;
+                // 是否遍历完模式串
+                if (j == pattern.length() - 1) {
+                    return i;
+                }
             }
         }
         return -1;
@@ -45,15 +42,12 @@ public class StringMatch {
         // 先对主串进行一次hash
         int strHash = hash(str.substring(0, pattern.length()));
 
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < str.length() - pattern.length(); i++) {
             String subStr = str.substring(i, i + pattern.length());
             if (strHash == patternHash && subStr.equals(pattern)) {
                 return i;
             }
 
-            if (i + pattern.length() >= str.length()) {
-                break;
-            }
             // 增量式hash
             strHash -= str.charAt(i) - 'a';
             strHash += str.charAt(i + pattern.length()) - 'a';
@@ -71,11 +65,55 @@ public class StringMatch {
     }
 
 
-    // BM KMP Sunday Horspool s
+    // BM KMP Horspool AC
     public static int matchByBM(String str, String pattern) {
         return -1;
     }
 
 
+    /**
+     * Sunday算法, BM的优化
+     * 由前往后遍历主串，不匹配时关注主串中位于模式串后的第一个字符
+     * 再在模式串中从后往前查找该字符
+     * 若没有该字符，则把模式串移动到该字符后
+     * 若有该字符，则移动模式串使其对齐
+     *
+     * 时间O(m/n), 特殊情况下会退化成O(m*n)
+     * 例如 主串：aaaaaabaaaaabaaaaab  模式串：aaaaa
+     * 每次失败时只向后移动一位
+     */
+    public static int matchBySunday(String str, String pattern) {
+        for (int i = 0; i < str.length() - pattern.length(); i++) {
+            for (int j = 0; j < pattern.length(); j++) {
+                if (str.charAt(i + j) != pattern.charAt(j)) {
+                    char c = str.charAt(i + pattern.length());
+                    int idx;
+                    if ((idx = indexOfSubstr(pattern, c)) == -1) {
+                        // 不存在该字符，移动子串长度+1位
+                        i += pattern.length();
+                    } else {
+                        // 存在时, 移动位数 = 模式串长度 - 该字符最右出现的位置(以0开始)
+                        // = 模式串中该字符最右出现的位置到尾部的距离 + 1
+                        i += pattern.length() - idx - 1;
+                    }
+                    break;
+                }
 
+                if (j == pattern.length() - 1) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    // 从后往前查找字符在字符串中出现的位置
+    private static int indexOfSubstr(String str, char c) {
+        for (int i = str.length() - 1; i >= 0; i--) {
+            if (str.charAt(i) == c) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
--- src/test/java/StrMatchTest.java
+++ src/test/java/StrMatchTest.java
@@ -1,27 +1,41 @@
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import utils.StrMatchUtil;
 
-import static algo.string.StringMatch.matchByBF;
-import static algo.string.StringMatch.matchByRK;
+import static algo.string.StringMatch.*;
 
 public class StrMatchTest {
 
+    private static StrMatchUtil matchUtil;
+    private static String pattern;
+    @BeforeClass
+    public static void init() {
+        matchUtil = new StrMatchUtil();
+        pattern = "&%^*^$$$$$$$$$$$$$***********************************((((((((($$$$$$$$$^^^^^^^^^^^^^^^^^*****************############";
+    }
+
     @Test
     public void testBF() {
-        for (int i = 0; i < 100; i++) {
-            StrMatchUtil matchUtil = new StrMatchUtil();
-            String s = matchUtil.create(10000, "&&((*&");
-            Assert.assertTrue(matchUtil.isMatch(matchByBF(s, "&&((*&")));
+        for (int i = 0; i < 10000; i++) {
+            String s = matchUtil.create(1000, pattern);
+            Assert.assertTrue(matchUtil.isMatch(matchByBF(s, pattern)));
         }
     }
 
     @Test
     public void testRK() {
-        for (int i = 0; i < 100; i++) {
-            StrMatchUtil matchUtil = new StrMatchUtil();
-            String s = matchUtil.create(10000, "!!!");
-            Assert.assertTrue(matchUtil.isMatch(matchByRK(s, "!!!")));
+        for (int i = 0; i < 10000; i++) {
+            String s = matchUtil.create(1000, pattern);
+            Assert.assertTrue(matchUtil.isMatch(matchByRK(s, pattern)));
+        }
+    }
+
+    @Test
+    public void testSunday() {
+        for (int i = 0; i < 10000; i++) {
+            String s = matchUtil.create(1000, pattern);
+            Assert.assertTrue(matchUtil.isMatch(matchBySunday(s, pattern)));
         }
     }
 }
